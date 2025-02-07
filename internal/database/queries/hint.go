5d99d539a501f01c5d3e9d5a09dfe53d61e97ca1
likesense <likesensed@gmail.com>
2025-02-07 22:11:44 +0300

    (feat): Added new queries for hints and tasks

--- internal/database/queries/hint.go
+++ internal/database/queries/hint.go
@@ -12,4 +12,18 @@ const (
 	WHERE h.task_id = $1
 	ORDER BY h.id
 	`
+	CreateNewHint = `
+	INSERT INTO eduroam.public.hint (task_id, theme, hint_text)
+	VALUES ($1, $2, $3)
+	RETURNING id, task_id, theme, hint_text, is_used 
+	`
+	UpdateHintByID = `
+	UPDATE eduroam.public.hint h
+	SET h.theme = COALESCE($1, h.theme)
+		h.task_id = COALESCE($1, h.theme)
+		h.is_used = COALESCE($1, h.theme)
+		h.hint_text = COALESCE($1, h.theme)
+	WHERE h.id = $5
+	RETURNING h.id, h.task_id, h.theme, h.hint_text, h.is_used
+	`
 )

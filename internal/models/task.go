4c1704ba25b021ea22f2014e926417891c8cec2f
Закомалдин Иван Анатольевич <i.a.zakomaldin@mgts.ru>
2025-03-29 11:46:06 +0300

    (chore): updated model

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,10 +1,11 @@
 package models
 
 type Task struct {
-	ID         uint64 `json:"id" db:"id"`
-	Theme      string `json:"theme" db:"theme"` //тема задачи
-	IsFinished bool   `json:"is_finished" db:"is_finished"`
-	TaskText   string `json:"task_text" db:"task_text"`
-	Attempts   uint16 `json:"attempts" db:"attempts"`     //попытки
-	Complexity uint16 `json:"complexity" db:"complexity"` //сложность
+	ID           uint64 `json:"id" db:"id"`
+	Theme        string `json:"theme" db:"theme"` //тема задачи
+	IsFinished   bool   `json:"is_finished" db:"is_finished"`
+	TaskText     string `json:"task_text" db:"task_text"`
+	Attempts     uint16 `json:"attempts" db:"attempts"`     //попытки
+	Complexity   uint16 `json:"complexity" db:"complexity"` //сложность
+	CourseTaskID uint64 `json:"course_task_id" db:"course_task_id"`
 }
